8b295cf90cd9b5952b0e2cd1449311cf191b33d5
TsubasaKogoma <tsubasa.kogoma@axia-work.com>
2021-12-12 12:56:12 +0900

    Initial Commit

---
+++ MultiProjectTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MultiProjectTest
+//
+//  Created by TsubasaKogoma on 2021/12/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MultiProjectTest/MultiProjectTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiProjectTestApp.swift
+//  MultiProjectTest
+//
+//  Created by TsubasaKogoma on 2021/12/12.
+//
+
+import SwiftUI
+
+@main
+struct MultiProjectTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
